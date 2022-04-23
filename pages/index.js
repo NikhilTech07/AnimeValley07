d03ec4622ba6ee7dee10dc94894e48f3c4bc1841
Nikhil9hac <kumarnikhilsingh.hac07@gmail.com>
2022-04-23 13:01:33 +0530

    more button

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,27 @@ import Link from "next/link";
 import PopularAnimeAside from "../components/PopularAnimeAside";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { HiDotsVertical } from "react-icons/hi";
+import ReactDOMServer from 'react-dom/server';
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import { Autoplay, EffectCoverflow } from "swiper";
 import { FaPlay } from "react-icons/fa";
-import { useState, useRef,useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
+let page = 1;
+let newIndex = 0;
 export const getServerSideProps = async (context) => {
-  const url=context.req.headers.host;
+  const url = context.req.headers.host;
   try {
     const carouselRes = await fetch(`http://${url}/api/carouselAnime`, {
       method: "Post"
     })
     const carouselData = await carouselRes.json();
-    const recentAnimeRes=await fetch(`http://${url}/api/displayAnime`,{
-      method:"Post"
+    const recentAnimeRes = await fetch(`http://${url}/api/displayAnime`, {
+      method: "Post"
     });
-    const recentAnimeData=await recentAnimeRes.json();
+    const recentAnimeData = await recentAnimeRes.json();
     const popularAnimeAsideTodayRes = await fetch(`http://${url}/api/popularAnimeListToday`, {
       method: 'post'
     });
@@ -32,8 +36,8 @@ export const getServerSideProps = async (context) => {
       method: 'post'
     });
     const popularAnimeAsideMonthData = await popularAnimeAsideMonthRes.json();
-    return{
-      props:{
+    return {
+      props: {
         carouselData,
         popularAnimeAsideMonthData,
         popularAnimeAsideWeekData,
@@ -42,79 +46,104 @@ export const getServerSideProps = async (context) => {
       }
     }
   }
-  catch{
+  catch {
     console.log("error")
   }
 }
-const Index = ({carouselData,recentAnimeData,popularAnimeAsideTodayData,popularAnimeAsideMonthData,popularAnimeAsideWeekData}) => {
+const Index = ({ carouselData, recentAnimeData, popularAnimeAsideTodayData, popularAnimeAsideMonthData, popularAnimeAsideWeekData }) => {
   const animeContainer = useRef();
-  const animeText=useRef();
-  const animeCarousel=useRef();
-  const moreContainer=useRef();
-  const [animeDetails,SetanimeDetails]=useState({recentAnime:{},popularAnimeToday:{},popularAnimeWeek:{},popularAnimeMonth:{},ongoingAnime:{},animeMovie:{}});
+  const animeText = useRef();
+  const animeCarousel = useRef();
+  const animeMenu = useRef();
+  const moreContainer = useRef();
+  const [animeDetails, SetanimeDetails] = useState({ recentAnime: {}, popularAnimeToday: {}, popularAnimeWeek: {}, popularAnimeMonth: {}, ongoingAnime: {}, animeMovie: {} });
   const [popularAnimeType, SetPopularAnimeType] = useState("today");
-  const [animeContainerContent,SetanimeContainerContent]=useState("recentAnime")
-  let page=1;
- 
-  useEffect(()=>{
-    const reloadPageFunc=async()=>{
-     
-      const ongoingAnimeRes=await fetch("/api/ongoingAnime",{
-        method:"Post"
-      });
-      const ongoingAnimeData=await ongoingAnimeRes.json();
-      const animeMovieRes=await fetch("/api/animeMovie",{
-        method:"Post"
-      });
-      const animeMovieData=await animeMovieRes.json();
-      SetanimeDetails({
-        recentAnime:recentAnimeData,
-        ongoingAnime:ongoingAnimeData,
-        animeMovie:animeMovieData
-      })
-    }
+  const [animeContainerContent, SetanimeContainerContent] = useState("recentAnime")
+  const reloadPageFunc = async () => {
+
+    const ongoingAnimeRes = await fetch("/api/ongoingAnime", {
+      method: "Post"
+    });
+    const ongoingAnimeData = await ongoingAnimeRes.json();
+    const animeMovieRes = await fetch("/api/animeMovie", {
+      method: "Post"
+    });
+    const animeMovieData = await animeMovieRes.json();
+    SetanimeDetails({
+      recentAnime: recentAnimeData,
+      ongoingAnime: ongoingAnimeData,
+      animeMovie: animeMovieData
+    })
+  }
+  useEffect(() => {
     reloadPageFunc()
-  },[])
+  }, [])
   const addPage = async (name) => {
-    page=page+1;
+    page = page + 1;
     try {
-      console.log(page);
+      const container = moreContainer.current;
       const res = await fetch(`/api/pagesApi/${name}?pages=${page}`);
-      const container=moreContainer.current;
       const data = await res.json();
-      data.map((val) => {
-        let newAnimeCard=document.createElement('div');
-        newAnimeCard.className='anime_card'
-        newAnimeCard.setAttribute("key",`${val.name}`)
-        newAnimeCard.innerHTML = ` 
-                           <a href="/anime/S1/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}">
-                           <div class="anime_image">
-                           <Image src=${val.img} width="100px" height="100px" alt={val.name}></Image>
-                         </div>
-                         <div class="anime_desc">
-                           <p class="anime_title">${val.name}</p>
-                           <p class="anime_date">${val.date}</p>
-                         </div>
-                           <a/>
-                          `
-              container.appendChild(newAnimeCard)
+      data.map((val, index) => {
+        // let newAnimeCardGroup = document.createElement('div');
+        // newAnimeCardGroup.className = 'anime_card_details';
+        // newAnimeCardGroup.style.position = "relative";
+        // newAnimeCardGroup.setAttribute("key", `${val.name}`)
+        container.innerHTML+=` 
+        <div class="anime_card_details" key={val.name} style="position:relative">
+         <a href=/anime/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}>
+            <div class="anime_card">
+              <div class="anime_image">
+              <Image src=${val.img} width="100px" height="100px" alt=${val.name}></Image>
+              </div>
+              <div class="anime_desc">
+                <p class="anime_title">${val.name}</p>
+                <p class="anime_date">${val.date}</p>
+              </div>
+            </div>
+          </a>
+            <div class="button_anime_menu">
+                  <button class="anime_menu_icon" onclick="document.getElementsByClassName('anime_menu')[${index+newIndex}].style.display='block' " onblur="document.getElementsByClassName('anime_menu')[${index+newIndex}].style.display='none'">
+                  ${ReactDOMServer.renderToString(<HiDotsVertical />)}
+                  </button>
+            </div>
+            <div class="anime_menu" ref={animeMenu} onmouseleave="document.getElementsByClassName('anime_menu')[${index+newIndex}].style.display='none'">
+                      <ul class="anime_menu_items_widgets">
+                        <li class="anime_menu_widgets">
+                            <a href=/anime/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}>
+                            Play
+                            </a>
+                        </li>
+                        <li class="anime_menu_widgets">Add to Favourite</li>
+                        <li class="anime_menu_widgets">Add to Watch Later</li>
+                      </ul>
+                </div>
+      </div>`
       })
     } catch (error) {
       console.log(error)
     }
   }
-  const setContainer=(name)=>{
-    const carousel=animeCarousel.current;
-    const container=moreContainer.current;
-    const text=animeText.current;
-    if (name!=='recentAnime') {
-      carousel.style.display="none";
-      container.innerHTML=""
-      page=1;
-      text.innerText=name
+  const showAnimeMenu = (Index) => {
+    console.log(document.getElementsByClassName("anime_menu")[Index].innerHTML)
+    document.getElementsByClassName("anime_menu")[Index].style.display = "block";
+  }
+  const hideAnimeMenu = (Index) => {
+    document.getElementsByClassName("anime_menu")[Index].style.display = "none";
+  }
+  const setContainer = (name) => {
+    const carousel = animeCarousel.current;
+    const container = moreContainer.current;
+    const text = animeText.current;
+    newIndex=0;
+    if (name !== 'recentAnime') {
+      carousel.style.display = "none";
+      container.innerHTML = ""
+      page = 1;
+      text.innerText = name
     }
-    else{
-      carousel.style.display="block";
+    else {
+      carousel.style.display = "block";
     }
     SetanimeContainerContent(name);
   }
@@ -123,26 +152,26 @@ const Index = ({carouselData,recentAnimeData,popularAnimeAsideTodayData,popularA
       <header>
         <Nav />
         <div className="secondNavbar">
-                    <div className="widgets_list">
-                        <ul className="widgets_link">
-                        <li className='widgets' onClick={()=>{
-                            setContainer('recentAnime')
-                            }} >All</li>
-                            <li className='widgets' onClick={()=>{
-                               setContainer("ongoingAnime")
-                            }} >Ongoing</li>
-                            <li className='widgets' onClick={()=>{
-                              setContainer("popularAnime")
-                            }} >Popular</li>
-                            <li className='widgets' onClick={()=>{
-                              setContainer("animeMovie")
-                            }} >Movie</li>
-                            <li className='widgets' onClick={()=>{
-                              setContainer("animeSeries")
-                            }} >Series</li>
-                        </ul>
-                    </div>
+          <div className="widgets_list">
+            <ul className="widgets_link">
+              <li className='widgets' onClick={() => {
+                setContainer('recentAnime')
+              }} >All</li>
+              <li className='widgets' onClick={() => {
+                setContainer("ongoingAnime")
+              }} >Ongoing</li>
+              <li className='widgets' onClick={() => {
+                setContainer("popularAnime")
+              }} >Popular</li>
+              <li className='widgets' onClick={() => {
+                setContainer("animeMovie")
+              }} >Movie</li>
+              <li className='widgets' onClick={() => {
+                setContainer("animeSeries")
+              }} >Series</li>
+            </ul>
           </div>
+        </div>
         <div className='anime_carousel' ref={animeCarousel}>
           <Swiper
             effect={"coverflow"}
@@ -170,58 +199,77 @@ const Index = ({carouselData,recentAnimeData,popularAnimeAsideTodayData,popularA
               return (
                 <>
                   <SwiperSlide key={val.name}>
-                  <Link href={`/anime/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}`}>
-                    <a>
-                    <div className="carousel_card">
-                      <div className="carousel_image">
-                        <Image src={val.img} width="432px" height="286px" alt={val.name} />
-                      </div>
-                      <div className="carousel_anime_details">
-                        <p className="carousel_anime_name">{val.name}</p>
-                        <button className="carousel_play_button">Play <FaPlay style={{ color: "blue" }} /> </button>
-                      </div>
-                    </div>
-                    </a>
-                  </Link>
+                    <Link href={`/anime/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}`}>
+                      <a>
+                        <div className="carousel_card">
+                          <div className="carousel_image">
+                            <Image src={val.img} width="432px" height="286px" alt={val.name} />
+                          </div>
+                          <div className="carousel_anime_details">
+                            <p className="carousel_anime_name">{val.name}</p>
+                            <button className="carousel_play_button">Play <FaPlay style={{ color: "blue" }} /> </button>
+                          </div>
+                        </div>
+                      </a>
+                    </Link>
                   </SwiperSlide>
                 </>
               )
-            }):<p style={{color:"#fff",textAlign:"center"}}>Loading.....</p>} 
+            }) : <p style={{ color: "#fff", textAlign: "center" }}>Loading.....</p>}
           </Swiper>
         </div>
       </header>
-         <div className="megaContainer">
+      <div className="megaContainer">
         <section>
           <div className="TextAndBox">
             <div className="box"></div>
             <p className="writtenAnimeText" ref={animeText}>Recent Anime</p>
           </div>
           <main className="animeContainer" ref={animeContainer}>
-            {animeDetails[`${animeContainerContent}`] ? Array.from(animeDetails[`${animeContainerContent}`]).map((val) => {
+            {animeDetails[`${animeContainerContent}`] ? Array.from(animeDetails[`${animeContainerContent}`]).map((val, Index) => {
               return (
                 <>
-                  <Link href={`/anime/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}`}>
-                    <a>
-                      <div className="anime_card" key={val.name}>
-                        <div className="anime_image">
-                          <Image src={val.img} width="100px" height="100px" alt={val.name}></Image>
+                  <div className="anime_card_details" key={val.name} style={{ position: "relative" }}>
+                    <Link href={`/anime/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}`}>
+                      <a>
+                        <div className="anime_card">
+                          <div className="anime_image">
+                            <Image src={val.img} width="100px" height="100px" alt={val.name}></Image>
+                          </div>
+                          <div className="anime_desc">
+                            <p className="anime_title">{val.name}</p>
+                            <p className="anime_date">{val.date}</p>
+                          </div>
                         </div>
-                        <div className="anime_desc">
-                          <p className="anime_title">{val.name}</p>
-                          <p className="anime_date">{val.date}</p>
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
+                      </a>
+                    </Link>
+                    <div className="button_anime_menu">
+                      <button className="anime_menu_icon" onClick={() => { showAnimeMenu(Index) }} onBlur={() => hideAnimeMenu(Index)}><HiDotsVertical /></button>
+                    </div>
+                    <div className="anime_menu" ref={animeMenu} onMouseLeave={() => hideAnimeMenu(Index)}>
+                      <ul className="anime_menu_items_widgets">
+                        <li className="anime_menu_widgets">
+                          <Link href={`/anime/watch/${val.name.replaceAll(" ", '-').replaceAll("-Episode-", '?episode=').toLowerCase()}`}>
+                            <a>Play</a>
+                          </Link>
+                        </li>
+                        <li className="anime_menu_widgets">Add to Favourite</li>
+                        <li className="anime_menu_widgets">Add to Watch Later</li>
+                      </ul>
+                    </div>
+                  </div>
                 </>
               )
-            }):<p style={{color:"#fff",textAlign:"center"}}>Loading.....</p>}
+            }) : <p style={{ color: "#fff", textAlign: "center" }}>Loading.....</p>}
           </main>
           <main className={`animeContainer ${animeContainerContent}Container`} ref={moreContainer}>
 
           </main>
           <div className="button_for_more_anime_content">
-            <button className="more_anime_content" onClick={() =>{addPage(`${animeContainerContent}`)}}>More</button>
+            <button className="more_anime_content" onClick={() => {
+              newIndex = newIndex + 30,
+                addPage(`${animeContainerContent}`)
+            }}>More</button>
           </div>
         </section>
         <aside className="popularContainer">
@@ -236,31 +284,31 @@ const Index = ({carouselData,recentAnimeData,popularAnimeAsideTodayData,popularA
           </div>
           <main className="popularAnimeList">
             {popularAnimeType === "today" && <div className="popular_anime_today">
-              {popularAnimeAsideTodayData?popularAnimeAsideTodayData.map((val) => {
+              {popularAnimeAsideTodayData ? popularAnimeAsideTodayData.map((val) => {
                 return (
                   <>
                     <PopularAnimeAside name={val.name} img={val.img} views={val.views} key={val.name} />
                   </>
                 )
-              }):<p style={{color:"#fff",textAlign:"center"}}>Loading.....</p>}
+              }) : <p style={{ color: "#fff", textAlign: "center" }}>Loading.....</p>}
             </div>}
             {popularAnimeType === "week" && <div className="popular_anime_week">
-              {popularAnimeAsideWeekData?popularAnimeAsideWeekData.map((val) => {
+              {popularAnimeAsideWeekData ? popularAnimeAsideWeekData.map((val) => {
                 return (
                   <>
                     <PopularAnimeAside name={val.name} img={val.img} views={val.views} key={val.name} />
                   </>
                 )
-              }):<p style={{color:"#fff",textAlign:"center"}}>Loading.....</p>}
+              }) : <p style={{ color: "#fff", textAlign: "center" }}>Loading.....</p>}
             </div>}
             {popularAnimeType === "month" && <div className="popular_anime_Month" >
-              {popularAnimeAsideMonthData?popularAnimeAsideMonthData.map((val) => {
+              {popularAnimeAsideMonthData ? popularAnimeAsideMonthData.map((val) => {
                 return (
                   <>
                     <PopularAnimeAside name={val.name} img={val.img} views={val.views} key={val.name} />
                   </>
                 )
-              }):<p style={{color:"#fff",textAlign:"center"}}>Loading.....</p>}
+              }) : <p style={{ color: "#fff", textAlign: "center" }}>Loading.....</p>}
             </div>}
           </main>
         </aside>
