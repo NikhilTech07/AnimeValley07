ed071dbd4dd6977de6100c686c65949e7577ac93
Nikhil9hac <kumarnikhilsingh.hac07@gmail.com>
2022-05-19 11:22:09 +0530

    login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+    secret:process.env.JWT_SECRET,
     providers:[
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
