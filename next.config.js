d32e8e7dc14fcf9860c0b4efff08ddeb63b3d24e
Nikhil9hac <kumarnikhilsingh.hac07@gmail.com>
2022-05-28 11:55:38 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -6,12 +6,12 @@ const nextConfig = {
     ,'gogocdn.net','i.ibb.co','lh3.googleusercontent.com']
   },
   env:{
-    JWT_SECRET:process.env.JWT_SECRET,
-    DB_URL:process.env.DB_URL,
-    NEXTAUTH_URL:process.env.NEXTAUTH_URL,
-    NEXT_PUBLIC_SECRET:process.env.NEXT_PUBLIC_SECRET,
-    GOOGLE_ID:process.env.GOOGLE_ID,
-    GOOGLE_SECRET:process.env.GOOGLE_SECRET
+    JWT_SECRET:"thisisanimevalleywebsitefreeanimestreamingplatformforanimelovers",
+    DB_URL:"mongodb+srv://Nikhil:Nikhil1234500000@cluster0.6ay1m.mongodb.net/UserAccount?retryWrites=true&w=majority",
+    NEXTAUTH_URL:"http://animevalley07.herokuapp.com",
+    NEXT_PUBLIC_SECRET:"ece4ecec4901bc8e09af65f5c178e82b",
+    GOOGLE_ID:"765641074683-j2derr75ji44vtogj6nprj56v9g7p71s.apps.googleusercontent.com",
+    GOOGLE_SECRET:"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"
   }
 }
 
