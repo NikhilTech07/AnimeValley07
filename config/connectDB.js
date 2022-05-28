86afc1dc5bb735101cf8803ff80bdac1fd4b65e7
Nikhil9hac <kumarnikhilsingh.hac07@gmail.com>
2022-05-28 11:15:12 +0530

    deploment

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 const connectDB = () => {
-    mongoose.connect(`${process.env.DB_URL}`)
+    mongoose.connect("mongodb+srv://Nikhil:Nikhil1234500000@cluster0.6ay1m.mongodb.net/UserAccount?retryWrites=true&w=majority")
     .then(()=>{
         console.log("connection...")
     }).catch((err)=>{
