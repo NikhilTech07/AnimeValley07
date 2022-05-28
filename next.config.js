115841684a8bfad6921534958ef8eb37de269d22
Nikhil9hac <kumarnikhilsingh.hac07@gmail.com>
2022-05-28 11:50:49 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images:{
     domains:["cdnimg.xyz",'ww1.gogoanime2.org','ani-img.api-web.site','ww.9anime2.com','img.bunnycdnn.ru','www.animelandtv.me'
     ,'gogocdn.net','i.ibb.co','lh3.googleusercontent.com']
+  },
+  env:{
+    JWT_SECRET:process.env.JWT_SECRET,
+    DB_URL:process.env.DB_URL,
+    NEXTAUTH_URL:process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_SECRET:process.env.NEXT_PUBLIC_SECRET,
+    GOOGLE_ID:process.env.GOOGLE_ID,
+    GOOGLE_SECRET:process.env.GOOGLE_SECRET
   }
 }
 
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
-    secret:"fjfjdSHFUHSDBSUFbysvbhvbhjBvdsivb",
+    secret:process.env.NEXT_PUBLIC_SECRET,
     providers:[
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
