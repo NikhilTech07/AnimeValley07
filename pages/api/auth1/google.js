e194550a79d3084562fa48f22043f41ba8fb9967
Nikhil9hac <kumarnikhilsingh.hac07@gmail.com>
2022-05-30 00:15:23 +0530

    deploy

--- pages/api/auth1/google.js
+++ pages/api/auth1/google.js
@@ -25,7 +25,6 @@ const google=async(req,res)=>{
         const _date=new Date();
             res.setHeader("Set-Cookie",cookie.serialize('AnimeValley_token',token,{
                 httpOnly:true,
-                secure:process.env.NODE_ENV !== "development",
                 expires:new Date(_date.getFullYear()+3,_date.getMonth(),_date.getDate()),
                 sameSite:"strict",
                 path:"/"
